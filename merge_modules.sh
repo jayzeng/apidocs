c659f9f1683fa8deb9d4fdfd5c02c7dc7cde3bc5
Kiefer Hagin <kieferhagin@gmail.com>
2014-09-16 16:32:00 -0700

    fixed merge bash script

--- merge_modules.sh
+++ merge_modules.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 cat base/base.md > apiary.apib
-cat modules/*.md >> apiary.apib
+
+for f in modules/*.md
+do
+    echo >> apiary.apib
+    cat $f >> apiary.apib
+done
 
 echo "Merged modules"
